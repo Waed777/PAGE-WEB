9cfa35c8e11f296a3c351e45bf84f5981f02d5f6
waed <waedamari1@gmail.com>
2023-11-01 13:06:02 +0100

    Interraction between HTML,CSS,JAVA SECRIPT

---
+++ secript.js
@@ -0,0 +1,7 @@
+// JavaScript
+const showMessageButton = document.getElementById('showMessageButton');
+const message = document.getElementById('message');
+
+showMessageButton.addEventListener('click', () => {
+    message.style.display = 'block';
+});
